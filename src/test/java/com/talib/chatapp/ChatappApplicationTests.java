5c56a7ce32be1f0f33449c8fb4543f486d958e01
Talib <abutalib715@gmail.com>
2025-03-08 05:54:46 +0600

    first commit

---
+++ src/test/java/com/talib/chatapp/ChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talib.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
