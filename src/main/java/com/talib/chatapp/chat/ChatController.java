cecd81158c74a610fc46034e5bbe60858193e01f
Talib <abutalib715@gmail.com>
2025-03-09 00:28:30 +0600

    web socket chat app done

--- src/main/java/com/talib/chatapp/chat/ChatController.java
+++ src/main/java/com/talib/chatapp/chat/ChatController.java
@@ -15,7 +15,7 @@ public class ChatController {
         return chatMessage;
     }
 
-    @MessageMapping("/chat.adduser")
+    @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
         //Add Username to WS Session
