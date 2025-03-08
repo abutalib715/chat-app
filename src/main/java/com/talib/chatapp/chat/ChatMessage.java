a9c443206ec14b9ca9da2ec1dcf78ee49ef9d6ba
Talib <abutalib715@gmail.com>
2025-03-08 16:22:38 +0600

    chat controller added

---
+++ src/main/java/com/talib/chatapp/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.talib.chatapp.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/talib/chatapp/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.talib.chatapp.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVER
+}
